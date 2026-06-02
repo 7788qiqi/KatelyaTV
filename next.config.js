4a10c2d21c6c057dbbb8a45b8e224886b231579d
7788qiqi <7788qiqi@proton.me>
2026-06-02 14:11:20 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -66,14 +66,9 @@ const nextConfig = {
 
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'production' && !!process.env.CF_PAGES, 
+  disable: process.env.NODE_ENV === 'production', 
   register: true,
   skipWaiting: true,
 });
-
-module.exports = withPWA({
-  images: {
-    unoptimized: true,
-  },
   
 module.exports = withPWA(nextConfig);
